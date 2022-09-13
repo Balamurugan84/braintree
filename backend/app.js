49f22dff6ad916037392846a08075e5a3f9ecc8e
Balamurugan84 <102845708+Balamurugan84@users.noreply.github.com>
2022-09-12 22:53:29 -0700

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const braintree = require('braintree')
+const cors = require('cors');
+const { response } = require('express');
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+const config ={
+    environment:braintree.Environment.Sandbox,
+    merchantId:'pw9gkb87zjsjypkn',
+    publicKey:'jpnncypvdwzfwwmg',
+    privateKey:'0628f1bd948bdcb84d6235f9b1df6cbb'
+}
+
+const gateway = new braintree.BraintreeGateway(config)
+
+app.get('/token', async(req, res)=>{
+    try{
+        gateway.clientToken.generate({}, (err, response)=>{
+            if(err){
+                return res.send({'error':err})
+            }else{
+                return res.json({'status':'success', 'message':response})
+            }
+        })
+    }catch(err){
+        return res.json({'error':err.message})
+    }
+})
+
+
+app.post('/transaction', async(req,res)=>{
+    try {
+        const paymentDetail = gateway.transaction.sale({
+            amount:req.body.amount,
+            paymentMethodNonce:req.body.paymentMethodNonce,
+            options:{
+                submitForSettlement:true
+            }
+        },(err, response)=>{
+            if(response.success){
+                return res.json({'status':'success', 'message': response.transaction})
+            }else{
+                return res.send({err:err})
+            }
+        })
+    } catch (error) {
+        return res.send({error:error.message})
+    }
+})
+
+app.listen(7575, ()=>{
+    console.log("Started...")
+})
+
+
+//http://localhost:7575
+
